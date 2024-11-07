33e7ca1a7fbb4b4d9569ce49d93b4f361352e058
AmirC4b <amirg.ff999@gmail.com>
2024-11-07 20:02:56 +0330

    single product page done...

--- src/script.js
+++ src/script.js
@@ -13,12 +13,12 @@ async function renderMenuCategories() {
   const temp = cats
     .map((item) => {
       return `
-    <li class="pt-2">
-      <a href="/categories/${item}" onclick="handleAClick(event)">
-        ${item}
-      </a>
-    </li>
-    `;
+        <li class="pt-2">
+          <a href="/categories/${item}" onclick="handleAClick(event)">
+            ${item}
+          </a>
+        </li>
+      `;
     })
     .join("");
 
@@ -32,8 +32,7 @@ renderMenuCategories();
 async function getAllProductsByFilter(limit = "") {
   const result = await fetch(
     `https://fakestoreapi.com/products${limit ? `?limit=${limit}` : ""}`
-  )
-    .then((res) => res.json());
+  ).then((res) => res.json());
   return result;
 }
 
@@ -59,7 +58,7 @@ function renderProducts(list) {
       return `
         <div
           onclick="handleProductClick(${product.id})"
-          class="shadow-[0px_4px_10px_4px_#00000024] w-full rounded-md p-2 overflow-hidden"
+          class="shadow-[0px_4px_10px_4px_#00000024] w-full rounded-md p-2 overflow-hidden cursor-pointer"
         >
           <img
             src="${product.image}"
@@ -71,6 +70,11 @@ function renderProducts(list) {
             <div>
               <span>${product.price}</span>
               <span>Toman</span>
+              <div class="mt-4">
+                <button class="px-3 py-2 border border-black hover:bg-black hover:text-white duration-200 rounded-md">
+                  Add to Cart              
+                </button>
+              </div>
             </div>
           </div>
         </div>
@@ -123,15 +127,6 @@ function handleAClick(event) {
   checkState();
 }
 
-// Toggle Mobile Menu (open/close)
-function toggleMobileMenu(action) {
-  if (action === "open") {
-    mobileMenu.classList.remove("scale-x-0");
-  } else if (action === "close") {
-    mobileMenu.classList.add("scale-x-0");
-  }
-}
-
 // Get Single Product Data
 async function getSingleProduct(productId) {
   const result = await fetch(`https://fakestoreapi.com/products/${productId}`)
@@ -142,25 +137,37 @@ async function getSingleProduct(productId) {
 // Render Single Product (Product Details Page)
 function renderSingleProduct({ category, description, image, price, title }) {
   const template = `
-    <div class="w-11/12 mx-auto pt-16 flex flex-col gap-2 md:gap-4 md:max-w-[1280px] md:flex-row md:items-start">
-      <img src="${image}" class="rounded-md w-1/2 max-w-md hidden md:block" alt="${title}" />
-      <div class="order-1 w-full">
-        <span class="text-white bg-black rounded-full px-4 py-1">${category}</span>
+    <div class="w-11/12 mx-auto pt-12 flex flex-col gap-2 md:gap-4 md:max-w-[1280px] md:flex-row md:items-start">
+      <img src="${image}" class="rounded-md w-1/3 max-w-md hidden md:block" alt="${title}" />
+      <div class="order-1 w-full mt-5"> 
         <div class="mt-4">
-          <a href="/" onclick="handleAClick(event)">صفحه اصلی</a> / 
-          <a onclick="handleAClick(event)" href="/products">همه محصولات</a>
+          <span class="text-white bg-black rounded-full px-4 py-1">${category}</span>
+          <a href="/" onclick="handleAClick(event)">Home</a> / 
+          <a onclick="handleAClick(event)" href="/products">All Products</a>
         </div>
         <h1 class="text-slate-700 mt-4 text-2xl font-bold">${title}</h1>
         <img src="${image}" class="rounded-md w-full md:hidden block my-4" alt="${title}" />
         <div class="block text-center md:mt-4 md:text-start font-extrabold">
-          <span>${price}</span> تومان
+          <span>${price}</span> Toman
         </div>
-        <button class="w-full md:w-auto px-4 py-2 my-5 bg-green-400 text-white rounded-md">اضافه به سبد خرید</button>
-        <p>${description}</p>
+        <button class="w-full md:w-auto px-4 py-2 my-5 border-black border duration-200 rounded-md hover:bg-black hover:text-white">Add To Cart</button>
+        <p class="w-[30rem] my-3 flex justify-center items-center">${description}</p>
       </div>
     </div>
   `;
   root.innerHTML = template;
+}
+
+// Get Single Category Products
+async function getSingleCategory(cat) {
+  const result = await fetch(`https://fakestoreapi.com/products/category/${cat}`)
+    .then((res) => res.json());
+  return result;
+}
+
+// Render Single Category
+function renderSingleCategory(products) {
+  renderProducts(products);
 }
 
 // Check Current URL Path and Render Corresponding Content
